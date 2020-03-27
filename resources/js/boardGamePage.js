02ab220c62192250910f74f75cb9b787a8ea266c
Jørgen Årnes <36748061+jorgenaa@users.noreply.github.com>
2020-03-27 01:33:40 +0100

    changed the BEM namings & added more functionality with JS to the board game

---
+++ resources/js/boardGamePage.js
@@ -0,0 +1,143 @@
+var player1Total = 0,
+    player2Total = 0,
+    playerTurn = 1;
+
+//Fetching the localstorage content
+
+function getLocalStorageItems() {
+    let getPlayer1 = localStorage.getItem("Player1");
+    let getPlayer2 = localStorage.getItem("Player2");
+    let player1Token = JSON.parse(localStorage.getItem("Player1Token"));
+    let player2Token = JSON.parse(localStorage.getItem("Player2Token"));
+    document.getElementById("get-player1-token").src = player1Token;
+    document.getElementById("get-player2-token").src = player2Token;
+    document.querySelector(".get-player-1").innerHTML = getPlayer1;
+    document.querySelector(".get-player-2").innerHTML = getPlayer2;
+
+    let token1 = document.getElementById("token1");
+    token1.src = player1Token;
+    let token2 = document.getElementById("token2");
+    token2.src = player2Token;
+}
+getLocalStorageItems();
+
+//Tokens for the tiles
+
+var token1Wrapper = document.getElementById("token1-wrapper");
+var token2Wrapper = document.getElementById("token2-wrapper");
+token1Wrapper.style.display = "none";
+token2Wrapper.style.display = "none";
+
+//Tiles for the board game
+
+const totalTiles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+
+let tilesContainer = document.querySelector(".game__tiles-container");
+
+totalTiles.forEach(function(number) {
+    tilesContainer.innerHTML += `<div class="game__tile" id="${number}"><span class="game__number">${number}</span></div>`;
+});
+
+
+
+// Array of traps
+
+const traps = [{
+        enemyTrap1: "Pay tax penalty and go one step back"
+    },
+    {
+        enemyTrap2: "You are starving. Go two steps back and find something to eat"
+    },
+    {
+        enemyTrap3: "Deanerys’s Dragons have blocked the road ahead. Go three steps back"
+    },
+    {
+        enemyTrap4: "Some local farmers are in great danger and need your help. Go four steps back"
+    },
+    {
+        enemyTrap5: "A storm is coming. Go five steps back and take shelter"
+    }
+];
+
+
+//Dice roll event function
+
+document.querySelector("#dice").addEventListener("click", rollDice);
+
+function rollDice() {
+    var status = document.getElementById('status');
+    var diceResult = Math.floor(Math.random() * 6) + 1;
+    var dice = document.querySelector(".game__dice");
+    dice.style.display = "block";
+    dice.src = "../resources/graphics/icons/dice-" + diceResult + ".png";
+    dice.innerHTML = diceResult;
+
+    let tile;
+    // Token apperance condition
+    if (playerTurn === 1) {
+        player1Total = player1Total + diceResult;
+        playerTurn = 2;
+        token1Wrapper.style.display = "block";
+        tile = document.getElementById(player1Total);
+        tile.appendChild(token1Wrapper);
+
+    } else if (playerTurn === 2) {
+        player2Total = player2Total + diceResult;
+        playerTurn = 1;
+        token2Wrapper.style.display = "block";
+        tile = document.getElementById(player2Total);
+        tile.appendChild(token2Wrapper);
+    }
+    // Traps condition
+    if (player1Total === totalTiles[4]) {
+        status.innerHTML = traps[0].enemyTrap1;
+        player1Total = player1Total - 1;
+    } else if (player2Total === totalTiles[4]) {
+        status.innerHTML = traps[0].enemyTrap1;
+        player2Total = player2Total - 1;
+
+    } else if (player1Total === totalTiles[7]) {
+        status.innerHTML = traps[1].enemyTrap2;
+        player1Total = player1Total - 2;
+        //traps[0].enemyTrap1 = "";
+    } else if (player2Total === totalTiles[7]) {
+        status.innerHTML = traps[1].enemyTrap2;
+        player2Total = player2Total - 2;
+        //traps[0].enemyTrap1 = "";
+
+    } else if (player1Total === totalTiles[16]) {
+        status.innerHTML = traps[2].enemyTrap3;
+        player1Total = player1Total - 3;
+        //traps[1].enemyTrap2 = "";
+    } else if (player2Total === totalTiles[16]) {
+        status.innerHTML = traps[2].enemyTrap3;
+        player2Total = player1Total - 3;
+        //traps[1].enemyTrap2 = "";
+
+    } else if (player1Total === totalTiles[21]) {
+        status.innerHTML = traps[3].enemyTrap4;
+        player1Total = player1Total - 4;
+        //traps[2].enemyTrap3 = "";
+    } else if (player2Total === totalTiles[21]) {
+        status.innerHTML = traps[3].enemyTrap4;
+        player2Total = player2Total - 4;
+        //traps[2].enemyTrap3 = "";
+
+    } else if (player1Total === totalTiles[25]) {
+        status.innerHTML = traps[4].enemyTrap5;
+        player1Total = player1Total - 5;
+        traps[3].enemyTrap4 = "";
+    } else if (player2Total === totalTiles[25]) {
+        status.innerHTML = traps[4].enemyTrap5;
+        player2Total = player2Total - 5;
+        //traps[3].enemyTrap4 = "";
+
+    } else if (player1Total === totalTiles[31] || player2Total === totalTiles[31]) {
+        $('#myModal').modal('show')
+    } else {
+        status.innerHTML = "";
+    }
+
+    console.log("player1Total", player1Total);
+    console.log("player2Total", player2Total);
+}
---
+++ resources/js/index.js
@@ -0,0 +1,206 @@
+//Characters array
+
+const gameCharacters = [{
+		name: 'Eddard Stark',
+		allegiance: 'House Stark',
+		culture: 'Northmen',
+		icon: 'graphics/icons/wolf.png',
+		position: 'Lord of Winterfell',
+		strength: 'Skilled warrior'
+	},
+	{
+		name: 'Brynden Tully',
+		allegiance: 'House Tully',
+		culture: 'Andal',
+		icon: 'graphics/icons/food-chain.png',
+		position: 'Knight & battle commander',
+		strength: 'Defensive lord'
+	},
+	{
+		name: 'Tywin Lannister',
+		allegiance: 'House Lannister',
+		culture: 'Andal',
+		icon: 'graphics/icons/lion.png',
+		position: 'Head of House Lannister',
+		strength: 'Pragmatic & calculated warrior'
+	},
+	{
+		name: 'Robert Baratheon',
+		allegiance: 'House Baratheon',
+		culture: 'Andal',
+		icon: 'graphics/icons/stag_head.png',
+		position: 'King of the Andals and the First Men',
+		strength: 'Great warrior'
+	},
+	{
+		name: 'Viserys Targaryen',
+		allegiance: 'House Targaryen',
+		culture: 'Valyrian',
+		icon: 'graphics/icons/hydra.png',
+		position: 'King of the Andals and the First',
+		strength: 'Aggressive and immoral lord'
+	},
+	{
+		name: 'Balon Greyjoy',
+		allegiance: 'House Greyjoy',
+		culture: 'Ironborn',
+		icon: 'graphics/icons/giant-squid.png',
+		position: 'King of the Iron Islands',
+		strength: 'Hard, ambitious and ruthless lord'
+	},
+	{
+		name: 'Loras Tyrell',
+		allegiance: 'House Tyrell',
+		culture: 'Andal',
+		icon: 'graphics/icons/rose.png',
+		position: 'Lord Commander of the Kingsguard',
+		strength: 'Skilled knight'
+	},
+	{
+		name: 'Robin Arryn',
+		allegiance: 'House Arryn',
+		culture: 'Andal',
+		icon: 'graphics/icons/falcon-moon.png',
+		position: 'Lord of the Eyrie',
+		strength: 'Defensive lord'
+	},
+	{
+		name: 'Walder Frey',
+		allegiance: 'House Frey',
+		culture: 'Andal',
+		icon: 'graphics/icons/frey.png',
+		position: 'Lord of the Crossing and head of House Frey',
+		strength: 'Vice and brave lord'
+	},
+	{
+		name: 'Doran Martell',
+		allegiance: 'House Martell',
+		culture: 'Dornishmen',
+		icon: 'graphics/icons/sun-spear.png',
+		position: 'Prince of Dorne and Lord of Sunspear',
+		strength: 'Strategic abilities'
+	}
+];
+
+// created cards with template literals in a for loop
+let container = document.querySelector('.game__body');
+
+for (let i = 0; i < gameCharacters.length; i++) {
+	container.innerHTML += `<div class="col-10 col-md-6 col-lg-4 p-2">
+							<div class="card game__card game__card--color p-2" data-name="${gameCharacters[i].name}" data-icon="${gameCharacters[i].icon}"> 
+								<div class="game-body white-text">
+									<img class="game__icon" src="${gameCharacters[i].icon}" >
+									<p class="selected-player"></p>
+									<table class="game__table">
+										<tr>
+											<th>Name:</th> 
+											<td> ${gameCharacters[i].name}</td>
+										</tr>
+										<tr>
+											<th>Allegiance:</th> 
+											<td>${gameCharacters[i].allegiance}</td>
+										</tr>
+										<tr>
+											<th>Culture:</th> 
+											<td>${gameCharacters[i].culture}</td>
+										</tr>
+										<tr>
+											<th>Position:</th> 
+											<td>${gameCharacters[i].position}</td>
+										</tr>
+										<tr>
+											<th>Strength:</th> 
+											<td>${gameCharacters[i].strength}</td>
+										</tr>
+									</table>
+									
+								</div><!-- game-body -->
+							</div><!-- game -->
+						</div><!-- col -->
+				`;
+}
+
+let cards = document.querySelectorAll('.game__card');
+let player1 = null;
+let player2 = null;
+
+//Toggle & add content to localstorage function
+
+cards.forEach(function(el) {
+	el.addEventListener('click', activateCard, false);
+});
+
+function activateCard(event) {
+	if (
+		player1 &&
+		player2 &&
+		!this.classList.contains('game__card--active-player1') &&
+		!this.classList.contains('game__card--active-player2')
+	) {
+		return;
+	}
+
+	const selectedPlayerOne = document.querySelector('.selected-player-one');
+	const selectedPlayerTwo = document.querySelector('.selected-player-two');
+	const selectedPlayer = this.querySelector('.selected-player');
+
+
+	if (player1 === null && !this.classList.contains('game__card--active-player2')) {
+		player1 = this.dataset.name;
+		player1 = this.dataset.icon;
+		this.classList.add('game__card--active-player1');
+		this.classList.add('selected-player--active');
+		selectedPlayer.innerHTML = 'This is player 1';
+		selectedPlayerOne.innerHTML = this.dataset.name;
+		localStorage.setItem('Player1', this.dataset.name);
+		localStorage.setItem('Player1Token', JSON.stringify(this.dataset.icon));
+	} else {
+		if (this.classList.contains('game__card--active-player1')) {
+			player1 = null;
+			this.classList.remove('game__card--active-player1');
+			this.classList.remove('selected-player--active');
+			selectedPlayer.innerHTML = '';
+			selectedPlayerOne.innerHTML = '';
+			localStorage.removeItem('Player1');
+			localStorage.removeItem('Player1Token');
+		} else {
+			if (this.classList.contains('game__card--active-player2')) {
+				player2 = null;
+				this.classList.remove('game__card--active-player2');
+				this.classList.remove('selected-player--active');
+				selectedPlayer.innerHTML = '';
+				selectedPlayerTwo.innerHTML = '';
+				localStorage.removeItem('Player2');
+				localStorage.removeItem('Player2Token');
+			} else {
+				player2 = this.dataset.name;
+				player2 = this.dataset.icon;
+				this.classList.add('game__card--active-player2');
+				this.classList.add('selected-player--active');
+				selectedPlayer.innerHTML = 'This is player 2';
+				selectedPlayerTwo.innerHTML = this.dataset.name;
+				localStorage.setItem('Player2', this.dataset.name);
+				localStorage.setItem('Player2Token', JSON.stringify(this.dataset.icon));
+			}
+		}
+	}
+
+	toggleContinueButton();
+
+	event.stopPropagation();
+}
+
+//Continue game function
+
+let continueButton = document.querySelector('#continue');
+continueButton.addEventListener('click', toggleContinueButton);
+
+function toggleContinueButton() {
+	if (player1 !== null && player2 !== null) {
+		continueButton.disabled = false;
+		continueButton.classList.remove('game__btn--continue');
+	} else {
+		continueButton.disabled = true;
+		continueButton.classList.add('game__btn--continue');
+	}
+}
