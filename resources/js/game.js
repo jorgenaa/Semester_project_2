d9f1d9b93c2677d152763542102e5fe0e5a6f8fd
Jørgen Årnes <36748061+jorgenaa@users.noreply.github.com>
2020-04-02 16:08:54 +0200

    modified the dice function

--- resources/js/game.js
+++ resources/js/game.js
@@ -1,21 +1,20 @@
 //Fetching the localstorage content
 
 let getPlayer1 = localStorage.getItem('Player1'),
-    getPlayer2 = localStorage.getItem('Player2');
+	getPlayer2 = localStorage.getItem('Player2');
 
 (() => {
-    let player1Token = JSON.parse(localStorage.getItem('Player1Token'));
-    let player2Token = JSON.parse(localStorage.getItem('Player2Token'));
-    document.getElementById('get-player1-token').src = player1Token;
-    document.getElementById('get-player2-token').src = player2Token;
-    document.querySelector('.get-player-1').innerHTML = getPlayer1;
-    document.querySelector('.get-player-2').innerHTML = getPlayer2;
+	let player1Token = JSON.parse(localStorage.getItem('Player1Token'));
+	let player2Token = JSON.parse(localStorage.getItem('Player2Token'));
+	document.getElementById('get-player1-token').src = player1Token;
+	document.getElementById('get-player2-token').src = player2Token;
+	document.querySelector('.get-player-1').innerHTML = getPlayer1;
+	document.querySelector('.get-player-2').innerHTML = getPlayer2;
 
-    let token1 = document.getElementById('token1');
-    token1.src = player1Token;
-    let token2 = document.getElementById('token2');
-    token2.src = player2Token;
-
+	let token1 = document.getElementById('token1');
+	token1.src = player1Token;
+	let token2 = document.getElementById('token2');
+	token2.src = player2Token;
 })();
 //Tokens for the tiles
 
@@ -29,128 +28,107 @@ token2Wrapper.style.display = 'none';
 let tilesContainer = document.querySelector('.game__tiles-container');
 
 for (let i = 1; i < 31; i++) {
-    tilesContainer.innerHTML += `<div class="game__tile" id="${i}"><span class="game__number">${i}</span></div>`;
+	tilesContainer.innerHTML += `<div class="game__tile" id="${i}"><span class="game__number">${i}</span></div>`;
 }
 
 // Array of traps
 
-const traps = [{
-        enemyTrap1: 'Pay tax penalty and go one step back'
-    },
-    {
-        enemyTrap2: 'You are starving. Go two steps back and find something to eat'
-    },
-    {
-        enemyTrap3: 'Deanerys’s Dragons have blocked the road ahead. Go three steps back'
-    },
-    {
-        enemyTrap4: 'Some local farmers are in great danger and need your help. Go four steps back'
-    },
-    {
-        enemyTrap5: 'A storm is coming. Go five steps back and take shelter'
-    }
-];
+const traps = {
+	5: {
+		description: 'Pay tax penalty and go one step back',
+		penalty: 1
+	},
+	8: {
+		description: 'You are starving. Go two steps back and find something to eat',
+		penalty: 2
+	},
+
+	17: {
+		description: 'Deanerys’s Dragons have blocked the road ahead. Go three step back',
+		penalty: 3
+	},
+	22: {
+		description: 'Some local farmers are in great danger and need your help. Go four steps back',
+		penalty: 4
+	},
+
+	26: {
+		description: 'A storm is coming. Go five steps back and take shelter',
+		penalty: 5
+	}
+};
 
 //Dice roll event function
 
 let player1Total = 0,
-    player2Total = 0,
-    playerTurn = 1;
+	player2Total = 0,
+	playerTurn = 1;
 
 document.querySelector('#dice').addEventListener('click', rollDice);
 
 function rollDice() {
-    let status = document.getElementById('status');
-    let modal = document.querySelector('.game__modal');
-    let diceResult = Math.floor(Math.random() * 6) + 1;
-    let dice = document.querySelector('.game__dice');
-    dice.style.display = 'block';
-    dice.src = '../resources/graphics/icons/dice-' + diceResult + '.png';
-    dice.innerHTML = diceResult;
+	let status = document.getElementById('status');
+	let modal = document.querySelector('.game__modal');
+	let diceResult = Math.floor(Math.random() * 6) + 1;
+	let dice = document.querySelector('.game__dice');
+	dice.style.display = 'block';
+	dice.src = '../resources/graphics/icons/dice-' + diceResult + '.png';
+	dice.innerHTML = diceResult;
 
-    let tile;
+	let tile;
 
-    let tokenWinner = document.getElementById('token-winner');
-    let toggle1 = document.getElementsByClassName('game__token-toggle-1');
-    let toggle2 = document.getElementsByClassName('game__token-toggle-2');
+	let tokenWinner = document.getElementById('token-winner');
 
-    // Close modal & refresh page event
-    document.getElementById('close').addEventListener('click', function() {
-        modal.style.display = 'none';
-        window.location.reload();
-    });
+	// Close modal & refresh page event
+	document.getElementById('close').addEventListener('click', function(e) {
+		modal.style.display = 'none';
+		window.location.reload();
+		e.stopPropagation();
+	});
 
-    // Token apperance condition
-    if (playerTurn === 1) {
-        player1Total = player1Total + diceResult;
-        playerTurn = 2;
-        token1Wrapper.style.display = 'block';
-        tile = document.getElementById(player1Total);
-        tile.appendChild(token1Wrapper);
-    } else if (playerTurn === 2) {
-        player2Total = player2Total + diceResult;
-        playerTurn = 1;
-        token2Wrapper.style.display = 'block';
-        tile = document.getElementById(player2Total);
-        tile.appendChild(token2Wrapper);
-    }
 
-    // Traps condition
-    if (player1Total === 5) {
-        status.innerHTML = traps[0].enemyTrap1;
-        player1Total = player1Total - 1;
-    } else if (player2Total === 5) {
-        status.innerHTML = traps[0].enemyTrap1;
-        player2Total = player1Total - 1;
-    } else if (player1Total === 8) {
-        status.innerHTML = traps[1].enemyTrap2;
-        player1Total = player1Total - 2;
-        traps[0].enemyTrap1 = "";
-    } else if (player2Total === 8) {
-        status.innerHTML = traps[1].enemyTrap2;
-        player2Total = player2Total - 2;
-        traps[0].enemyTrap1 = "";
-    } else if (player1Total === 17) {
-        status.innerHTML = traps[2].enemyTrap3;
-        player1Total = player1Total - 3;
-        traps[1].enemyTrap2 = "";
-    } else if (player2Total === 17) {
-        status.innerHTML = traps[2].enemyTrap3;
-        player2Total = player1Total - 3;
-        traps[1].enemyTrap2 = "";
-    } else if (player1Total === 22) {
-        status.innerHTML = traps[3].enemyTrap4;
-        player1Total = player1Total - 4;
-        traps[2].enemyTrap3 = "";
-    } else if (player2Total === 22) {
-        status.innerHTML = traps[3].enemyTrap4;
-        player2Total = player2Total - 4;
-        traps[2].enemyTrap3 = "";
-    } else if (player1Total === 26) {
-        status.innerHTML = traps[4].enemyTrap5;
-        player1Total = player1Total - 5;
-        traps[3].enemyTrap4 = "";
-    } else if (player2Total === 26) {
-        status.innerHTML = traps[4].enemyTrap5;
-        player2Total = player2Total - 5;
-        traps[3].enemyTrap4 = "";
-    } else if (player1Total === 30 || player2Total > 31) {
-        modal.style.display = 'block';
-        player1Total = Math.round(30);
-        tokenWinner.innerHTML = getPlayer1;
-        toggle1.style.display = 'block';
-        toggle2.style.display = 'none';
-    } else if (player2Total === 30 || player2Total > 31) {
-        modal.style.display = 'block';
-        player2Total = Math.round(30);
-        tokenWinner.innerHTML = getPlayer2;
-        toggle2.style.display = 'block';
-        toggle1.style.display = 'none';
-    } else {
-        status.innerHTML = '';
-    }
+	// Token apperance condition
+	if (playerTurn === 1) {
+		player1Total = player1Total + diceResult;
+		playerTurn = 2;
+		token1Wrapper.style.display = 'block';
+		tile = document.getElementById(player1Total);
+		tile.appendChild(token1Wrapper);
+	} else if (playerTurn === 2) {
+		player2Total = player2Total + diceResult;
+		playerTurn = 1;
+		token2Wrapper.style.display = 'block';
+		tile = document.getElementById(player2Total);
+		tile.appendChild(token2Wrapper);
+	}
 
-    console.log('player1Total', player1Total);
-    console.log('player2Total', player2Total);
+	//Traps condition
+	if ('undefined' !== typeof traps[player1Total]) {
+		let player1Penalty = traps[player1Total];
+		status.innerHTML = player1Penalty.description;
+		player1Total -= player1Penalty.penalty;
+
+	} else if ('undefined' !== typeof traps[player2Total]) {
+		let player2Penalty = traps[player2Total];
+		status.innerHTML = player2Penalty.description;
+		player2Total -= player2Penalty.penalty;
+	} else if (player1Total >= 30) {
+		modal.style.display = 'block';
+		player1Total = Math.round(30);
+		tokenWinner.innerHTML = getPlayer1;
+	} else if (player2Total >= 30) {
+		modal.style.display = 'block';
+		player2Total = Math.round(30);
+		tokenWinner.innerHTML = getPlayer2;
+	} else {
+		status.innerHTML = '';
+	}
+
+	console.log('player1Total', player1Total);
+	console.log('player2Total', player2Total);
+}
+
+// function move token backwards
+function moveTokenback() {
 
 }
--- resources/js/winner.js
+++ resources/js/winner.js
@@ -1,8 +1,8 @@
-	var canvas = document.querySelector("canvas");
-	canvas.style.width = "100vw";
-	var canvasContext = canvas.getContext("2d");
-	var height = 10;
-	var width = 10;
-	var x = 0;
-	var y = 60;
-	var dx = 1;
+(() => {
+	let animationContainer = document.getElementById('animation-container');
+	for (let i = 0;) {
+
+	}
+}());
+
+//http://cssdeck.com/labs/fireflies
